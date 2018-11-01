7ddbd000de31dd3aaf72d5ef13836bfa11db7993
Shankar,K <kshan_77@yahoo.com>
2018-11-01 13:29:59 -0700

    Initial running version

---
+++ hdfs-only-3-node-hadoop-cluster/conf/myenv.sh
@@ -0,0 +1,5 @@
+export JAVA_HOME=$JAVA_HOME
+export HADOOP_HOME=/home/hadoop/hadoop-3.1.1
+export CLASSPATH=$CLASSPATH:$JAVA_HOME/lib:$JAVA_HOME/jre/lib
+export PATH=$JAVA_HOME/bin:$JAVA_HOME/jre/bin:$PATH:$HADOOP_HOME/bin
+export HADOOP_VERSION=3_1_1
