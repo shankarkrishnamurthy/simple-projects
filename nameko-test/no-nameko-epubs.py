395b45624d44b07cb3536012171b468f509caa93
Shankar,K <kshan_77@yahoo.com>
2019-01-09 12:08:13 -0800

    removing nameko-test

--- nameko-test/no-nameko-epubs.py
+++
@@ -1,8 +0,0 @@
-from nameko.standalone.events import event_dispatcher
-from time import sleep
-
-CONFIG = {'AMQP_URI': "amqp://guest:guest@localhost"}
-
-dispatch = event_dispatcher(CONFIG, mandatory=True)
-for i in range(3):
-    dispatch("friendlyservice", "hi", "payload whatever "+str(i))
--- nameko-test/timer.py
+++
@@ -1,9 +0,0 @@
-from nameko.timer import timer
-
-class Service:
-    name ="service"
-
-    @timer(interval=3)
-    def ping(self):
-        # method executed every 3 seconds
-        print("pong")
